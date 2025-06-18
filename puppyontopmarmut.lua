f82f67aa81bfa2ec5762a23242a1a5bb583fbe52
thangvo123 <94835684+thangvo123@users.noreply.github.com>
2025-06-18 11:44:46 +0700

    Update puppyontopmarmut.lua

--- puppyontopmarmut.lua
+++ puppyontopmarmut.lua
@@ -1,4 +1,21 @@
 _G.Config = { UserID = "3bf97fad-2915-4197-8a14-81872043402d", discord_id = "1165842656958611546" , Note = "PUPPY", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/bgsi"))()
+script_key = getgenv().AutoTrade;
+getgenv().Config = {
+    MainReceive = getgenv().MainReceiveUsers,
+    petList = {
+        ["Secret"] = {
+
+        },
+        ["normalPets"] = {
+
+        }
+    }
+}
+
+task.spawn(function()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/793674f6bc0a80e1c748fd75994d1607.lua"))()
+end)
+task.wait(120)
 script_key = getgenv().Marmot;
 setfpscap(3)  -- number = fps
 
