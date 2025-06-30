a8731f92046589ec718d0152bd905542b3ab6736
thangvo123 <94835684+thangvo123@users.noreply.github.com>
2025-06-30 23:20:38 +0700

    Update puppyontopmarmut.lua

--- puppyontopmarmut.lua
+++ puppyontopmarmut.lua
@@ -84,7 +84,7 @@ getgenv().Config = {
                        "Meerkat", "Sand Snake", "Fennec Fox", "Axolotl", "Hyacinth Macaw", "Hamster"},
     ["Destroy Mode"] = {
         ["Auto Destroy when have money"] = 1000000, -- its will destroy all trees select when money >= select
-        ["Trees"] = {"Strawberry", "Blueberry", "Corn", "Orange Tulip", "Carrot", "Tomato", "Wild Carrot", "Cantaloupe", "Pear", "Watermelon"}
+        ["Trees"] = {"Strawberry", "Blueberry", "Corn", "Orange Tulip", "Avocado", "Banana", "Green Apple", "Pineapple", "Kiwi", "Carrot", "Tomato", "Wild Carrot", "Cantaloupe", "Pear", "Watermelon"}
     },
     ["Url"] = "", -- Webhook 
 
