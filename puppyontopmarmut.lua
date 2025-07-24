01f1827bd60b3f2bccf120c4caa48d3d2fc2dc11
thangvo123 <94835684+thangvo123@users.noreply.github.com>
2025-07-24 16:58:38 +0700

    Update puppyontopmarmut.lua

--- puppyontopmarmut.lua
+++ puppyontopmarmut.lua
@@ -6,7 +6,7 @@ getgenv().gagConfig = {
     CRAFT_EVENT = { "Anti Bee Egg" },
     BUY_TRAVELING_MERCHANT = { "Bee Egg", "Loquat", "Feijoa", "Pitcher Plant" },
     BUY_EVENT_SHOP = { "Zen Egg" },
-    MAX_EVENT_RESTOCK_SHECKLES = 512000000,
+    MAX_EVENT_RESTOCK_SHECKLES = 1024000000,
     
     -- General:
     AUTO_UPDATE_RESTART = true,
