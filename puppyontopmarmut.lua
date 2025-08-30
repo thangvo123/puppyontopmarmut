4fb69bbf9c6fe97ae4e3a6c59175f58caeb74404
thangvo123 <94835684+thangvo123@users.noreply.github.com>
2025-08-30 23:35:45 +0700

    Update puppyontopmarmut.lua

--- puppyontopmarmut.lua
+++ puppyontopmarmut.lua
@@ -3,7 +3,7 @@ setfpscap(2)
 
 getgenv().gagConfig = {
     -- Event:
-    CRAFT_EVENT = { "Anti Bee Egg", "Sprout Egg", "Skyroot Chest" },
+    CRAFT_EVENT = { "Anti Bee Egg" },
     BUY_TRAVELING_MERCHANT = { "Bee Egg", "Paradise Egg", "Rare Summer Egg", "Loquat", "Feijoa", "Pitcher Plant" },
     CLAIM_FAIRY_REWARD = { "Enchanted Egg", "FairyPoints", "Pet Shard Glimmering", "Mutation Spray Glimmering", "Glimmering Radar" },
     PLANT_EVENT_TREES = true,  -- This config will replace most plant related config
